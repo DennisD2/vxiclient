e8c97a5d637161c9dfffcaca463e409e62708a85
dennis <dennis@dennis-pc.fritz.box>
2017-12-22 18:08:54 +0100

    Added select option for interval time

--- vxiclient/src/main/ng/vxiclient/src/app/app.component.ts
+++ vxiclient/src/main/ng/vxiclient/src/app/app.component.ts
@@ -12,24 +12,45 @@ import { AppRegistry } from './app.registry';
 })
 export class AppComponent implements OnInit {
   title = 'VXI Client';
+
   // interval length in [ms]
-  stepTime = 2000;
+  selectedStepTime = 2000;
+  allowedStepTimes = [ {id: 1, value: 1000}, {id: 2, value: 2000}, {id: 3, value: 10000}, {id: 4, value: 60000}, {id: 5, value: 600000 } ];
+  subscription: any;
 
   constructor(private appRegistry: AppRegistry) {
-    console.log("App CTR");
+    console.log('App CTR');
   }
 
   ngOnInit() {
-    IntervalObservable.create(this.stepTime).subscribe(() => { this.roll() });
+    this.setStepTime(2000);
   }
 
   /**
    * Do one measurement step in a Publish-Subscribe pattern.
    * For all active devices, do a measurement and publish the measurement result to all views.
-   * 
+   *
    */
   roll() {
     this.appRegistry.roll();
+  }
+
+  /**
+   * Set measurement intervall length.
+   *
+   * @param time intervall length in milliseconds.
+   */
+  setStepTime(time: number) {
+    console.log('Set steptime to: ' + time);
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
+    }
+    this.selectedStepTime = time;
+    this.subscription = IntervalObservable.create(this.selectedStepTime).subscribe(() => { this.roll(); });
+  }
+
+  getStepTime() {
+    return this.selectedStepTime;
   }
 
   reload() {
--- vxiclient/src/main/ng/vxiclient/src/app/app.registry.ts
+++ vxiclient/src/main/ng/vxiclient/src/app/app.registry.ts
@@ -7,76 +7,76 @@ import { Channel } from './types/Channel';
 
 @Injectable()
 export class AppRegistry {
-  views : View[] = new Array();
-  devices : Device[] = new Array();
-    
+  views: View[] = new Array();
+  devices: Device[] = new Array();
+
   constructor() { }
 
   /**
    * Subscribe a view. Views are data consumers in this scenario.
-   * 
+   *
    * @param dev view to subscribe.
    */
   subscribeView( view: View ) {
     if (this.views.includes(view)) {
-      console.log("view already subscribed: " +  view.getName())
+      console.log('view already subscribed: ' +  view.getName());
       return;
     }
     this.views.push(view);
-    console.log("view subscribed: " +  view.getName())
+    console.log('view subscribed: ' +  view.getName());
   }
 
   /**
    * Subscribe a device. Devices are ddata producers in this scenario.
-   * 
+   *
    * @param dev device to subscribe.
    */
   subscribeDevice( dev: Device ) {
     if (this.devices.includes(dev)) {
-      console.log("device already subscribed: " +  dev.getName())
+      console.log('device already subscribed: ' +  dev.getName());
       return;
     }
     this.devices.push(dev);
-    console.log("device subscribed: " +  dev.getName())
+    console.log('device subscribed: ' +  dev.getName());
   }
 
   /**
    * Unsubscribe a view.
-   * 
+   *
    * @param dev device to unsubscribe.
    */
   unsubscribeView( view: View ) {
     if (!this.views.includes(view)) {
-      console.log("view not subscribed: " +  view.getName())
+      console.log('view not subscribed: ' +  view.getName());
       return;
     }
-    var index = this.views.indexOf(view, 0);
+    const index = this.views.indexOf(view, 0);
     if (index > -1) {
       this.views.splice(index, 1);
     }
-    console.log("view unsubscribed: " +  view.getName())
+    console.log('view unsubscribed: ' +  view.getName())
   }
 
   /**
    * Unsubscribe a device.
-   * 
+   *
    * @param dev device to unsubscribe.
    */
   unsubscribeDevice( dev: Device ) {
     if (!this.devices.includes(dev)) {
-      console.log("device not subscribed: " +  dev.getName())
+      console.log('device not subscribed: ' +  dev.getName());
       return;
     }
-    var index = this.devices.indexOf(dev, 0);
+    const index = this.devices.indexOf(dev, 0);
     if (index > -1) {
       this.devices.splice(index, 1);
     }
-    console.log("device unsubscribed: " +  dev.getName())
+    console.log('device unsubscribed: ' +  dev.getName());
   }
 
   /*measure() {
     this.devices.map((d) => {
-      console.log("measure device: " + d.getName())
+      console.log('measure device: ' + d.getName())
       d.doMeasurementCallback();
     })
   */
@@ -87,9 +87,9 @@ export class AppRegistry {
    */
   publish(data: any) {
     this.views.map((v) => {
-      console.log("publish to view: " + v.getName())
+      console.log('publish to view: ' + v.getName());
       v.newSampleCallback(data);
-    })
+    });
   }
 
   /**
@@ -97,13 +97,13 @@ export class AppRegistry {
    */
   roll() {
     this.devices.map((d) => {
-      console.log("measure device: " + d.getName())
-      let data = d.doMeasurementCallback();
-      if (data != undefined) {
-        console.log("measured: " + JSON.stringify(data))
+      console.log('measure device: ' + d.getName());
+      const data = d.doMeasurementCallback();
+      if (data !== undefined) {
+        console.log('measured: ' + JSON.stringify(data));
        this.publish(data);
       }
-    })
+    });
   }
 
 }
