d18daaf128c8aa0b1fafeb1b350130bc60dc9537
dennis <dennis@dennis-pc.fritz.box>
2017-12-26 14:10:10 +0100

    Added *ngIfs to control display state of U elements

--- vxiclient/src/main/ng/vxiclient/src/app/devices/counter/counter.component.ts
+++ vxiclient/src/main/ng/vxiclient/src/app/devices/counter/counter.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseDevice } from '../base.device';
+import { Device } from '../../types/Device';
+import { AppRegistry } from '../../app.registry';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent  extends BaseDevice implements OnInit, Device {
 
-  constructor() { }
+  constructor(protected appRegistry: AppRegistry,
+    /*private multimeterService: MultimeterService*/) {
+      super(appRegistry);
+      this.type = 'none';
+      this.name = 'counter';
+      this.start();
+  }
 
   ngOnInit() {
   }
--- vxiclient/src/main/ng/vxiclient/src/app/devices/digital-io/digital-io.component.ts
+++ vxiclient/src/main/ng/vxiclient/src/app/devices/digital-io/digital-io.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseDevice } from '../base.device';
+import { Device } from '../../types/Device';
+import { AppRegistry } from '../../app.registry';
 
 @Component({
   selector: 'app-digital-io',
   templateUrl: './digital-io.component.html',
   styleUrls: ['./digital-io.component.css']
 })
-export class DigitalIOComponent implements OnInit {
+export class DigitalIOComponent extends BaseDevice implements OnInit, Device {
 
-  constructor() { }
+  constructor(protected appRegistry: AppRegistry,
+    /*private multimeterService: MultimeterService*/) {
+      super(appRegistry);
+      this.type = 'none';
+      this.name = 'digitalIO';
+      this.start();
+  }
 
   ngOnInit() {
   }
