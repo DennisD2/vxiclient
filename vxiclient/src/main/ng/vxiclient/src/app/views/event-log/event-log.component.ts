245156aef79aa82092bd378cf092a3bf61965c9b
dennis <dennis@dennis-pc.fritz.box>
2018-01-03 19:18:33 +0100

    EventLog printout of response data.

--- vxiclient/src/main/ng/vxiclient/src/app/views/event-log/event-log.component.ts
+++ vxiclient/src/main/ng/vxiclient/src/app/views/event-log/event-log.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { BaseView } from '../base.view';
 import { AppRegistry } from '../../app.registry';
 
+/**
+ * Event Logger View
+ *
+ * Displays request and response data in raw format (text).
+ */
 @Component({
   selector: 'app-event-log',
   templateUrl: './event-log.component.html',
   styleUrls: ['./event-log.component.css']
 })
 export class EventLogComponent extends BaseView implements OnInit {
+  request = '?';
+  response: string;
 
   constructor(protected appRegistry: AppRegistry) {
     super(appRegistry);
@@ -21,6 +28,7 @@ export class EventLogComponent extends BaseView implements OnInit {
 
   newSampleCallback(data: any) {
     console.log('new sample: ' + JSON.stringify(data));
+    this.response = JSON.stringify(data);
   }
 
 }
