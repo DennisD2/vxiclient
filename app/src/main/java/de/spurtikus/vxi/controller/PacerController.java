d26b56b0b37e2c4679f759154473aec4a91b0289
DennisD2 <info@spurtikus.de>
2019-10-06 18:27:52 +0200

    Added Pacer controller code

--- app/src/main/java/de/spurtikus/vxi/controller/PacerController.java
+++ app/src/main/java/de/spurtikus/vxi/controller/PacerController.java
@@ -1,4 +1,138 @@
 package de.spurtikus.vxi.controller;
 
-public class PacerController {
+import de.spurtikus.devices.hp.HP1300Pacer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * Controller for Pacer functionality built in in a mainframe.
+ * <p>
+ * Tested with:
+ * * HP 1300/1301. See class {HP1300Pacer}.
+ *
+ * @author dennis
+ */
+
+@RestController
+@RequestMapping("/api/rest/" + Constants.URL_PACER)
+public class PacerController extends AbstractController<HP1300Pacer> {
+    private Logger logger = LoggerFactory.getLogger(PacerController.class);
+
+    public PacerController() {
+        className = Constants.URL_PACER;
+    }
+
+    //@Produces(MediaType.TEXT_PLAIN)
+    @GetMapping("{mainframe}/{devname}/info")
+    public String info(@PathVariable("mainframe") String mainframe,
+                       @PathVariable("devname") String devname) {
+        //logger.debug("Incoming URI : {}", uriInfo.getPath());
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+        return getClassName();
+    }
+
+    @PostMapping("{mainframe}/{devname}/startSelfTriggered")
+    //@Consumes({ MediaType.APPLICATION_JSON })
+    //@Produces(MediaType.APPLICATION_JSON)
+    public String startSelfTriggered(@PathVariable("mainframe") String mainframe,
+                                     @PathVariable("devname") String devname) {
+        //logger.debug("Incoming URI : {}", uriInfo.getPath());
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+
+        try {
+            connManager = ConnectionManager.getInstance(this.getClass(), mainframe, devname);
+            getDevice(mainframe, devname).startPacerSelfTriggered();
+        } catch (Exception e) {
+            logger.error(
+                    "Cannot get wrapper instance. This is usually an initialization problem.");
+            return Response.Status.NOT_FOUND.getReasonPhrase();
+        }
+        return "OK";
+    }
+
+    @PostMapping("{mainframe}/{devname}/startExternalTriggered")
+    //@Consumes({ MediaType.APPLICATION_JSON })
+    //@Produces(MediaType.APPLICATION_JSON)
+    public String startExternalTrigger(@PathVariable("mainframe") String mainframe,
+                                       @PathVariable("devname") String devname) {
+        //logger.debug("Incoming URI : {}", uriInfo.getPath());
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+
+        try {
+            connManager = ConnectionManager.getInstance(this.getClass(), mainframe, devname);
+            getDevice(mainframe, devname).enablePacerExternalTrigger();
+        } catch (Exception e) {
+            logger.error(
+                    "Cannot get wrapper instance. This is usually an initialization problem.");
+            return Response.Status.NOT_FOUND.getReasonPhrase();
+        }
+        return "OK";
+    }
+
+    @PostMapping("{mainframe}/{devname}/stop")
+    //@Consumes({ MediaType.APPLICATION_JSON })
+    //@Produces(MediaType.APPLICATION_JSON)
+    public String stop(@PathVariable("mainframe") String mainframe,
+                       @PathVariable("devname") String devname) {
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+
+        try {
+            connManager = ConnectionManager.getInstance(this.getClass(), mainframe, devname);
+            getDevice(mainframe, devname).stopPacer();
+        } catch (Exception e) {
+            logger.error(
+                    "Cannot get wrapper instance. This is usually an initialization problem.");
+            return Response.Status.NOT_FOUND.getReasonPhrase();
+        }
+        return "OK";
+    }
+
+    @PostMapping("{mainframe}/{devname}/setCycles/{cycles}")
+    //@Consumes({ MediaType.APPLICATION_JSON })
+    //@Produces(MediaType.APPLICATION_JSON)
+    public String setCycles(@PathVariable("mainframe") String mainframe,
+                            @PathVariable("devname") String devname,
+                            @PathVariable("cycles") Long cycles) {
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+        logger.debug("Cycles: {}", cycles);
+
+        try {
+            connManager = ConnectionManager.getInstance(this.getClass(), mainframe, devname);
+            getDevice(mainframe, devname).setCycles(cycles);
+        } catch (Exception e) {
+            logger.error(
+                    "Cannot get wrapper instance. This is usually an initialization problem.");
+            return Response.Status.NOT_FOUND.getReasonPhrase();
+        }
+        return "OK";
+    }
+
+    @PostMapping("{mainframe}/{devname}/setPeriod/{period}")
+    //@Consumes({ MediaType.APPLICATION_JSON })
+    //@Produces(MediaType.APPLICATION_JSON)
+    public String setPeriod(@PathVariable("mainframe") String mainframe,
+                            @PathVariable("devname") String devname,
+                            @PathVariable("period") Double period) {
+        logger.debug("Mainframe: {}", mainframe);
+        logger.debug("Device name: {}", devname);
+        logger.debug("Period: {}", period);
+
+        try {
+            connManager = ConnectionManager.getInstance(this.getClass(), mainframe, devname);
+            getDevice(mainframe, devname).setPeriod(period);
+        } catch (Exception e) {
+            logger.error(
+                    "Cannot get wrapper instance. This is usually an initialization problem.");
+            return Response.Status.NOT_FOUND.getReasonPhrase();
+        }
+        return "OK";
+    }
 }
